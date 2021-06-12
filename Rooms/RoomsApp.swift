0f939d511a54233fac1098921ecdca3db3012ff3
Silvio Bulla <silviobulla12@gmail.com>
2021-06-12 20:46:36 +0200

    Initial Commit

---
+++ Rooms/RoomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomsApp.swift
+//  Rooms
+//
+//  Created by Silvio Bulla on 12.06.21.
+//
+
+import SwiftUI
+
+@main
+struct RoomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
