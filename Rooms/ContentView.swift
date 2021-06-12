39c43dd911e409efd7d4b281aff16c25af5e240e
Silvio Bulla <silviobulla12@gmail.com>
2021-06-12 22:43:50 +0200

    Adds room view

--- Rooms/ContentView.swift
+++ Rooms/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    var rooms: [Room] = []
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List(rooms) { room in
+                RoomCell(room: room)
+            }
+            .navigationBarTitle(Text("Rooms"))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(rooms: testData)
+    }
+}
+
+struct RoomCell: View {
+    let room: Room
+    var body: some View {
+        NavigationLink(destination: Text(room.name)) {
+            Image(room.thumbnailName)
+                .resizable()
+                .cornerRadius(5)
+                .scaledToFit()
+                .frame(width: 60, height: 60)
+
+            VStack(alignment: .leading) {
+                Text(room.name)
+                Text("\(room.capacity) people")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
     }
 }
